91cda7b047103bc4ca003bd58564e22797be5972
descaler <cpj.joyce@gmail.com>
2015-06-22 09:31:30 +0100

    Use scala option

--- src/main/scala/bowling/BowlingScorer.scala
+++ src/main/scala/bowling/BowlingScorer.scala
@@ -1,22 +1,20 @@
 package bowling
 
-import java.util.Optional
-
 sealed trait BowlingFrame {
   def b1: Int
-  def b2: Optional[Int]
-  def score:Int = b1+b2.orElse(0)
+  def b2: Option[Int]
+  def score:Int = b1+b2.getOrElse(0)
 }
 
-case class Frame(b1:Int, b2:Optional[Int]) extends BowlingFrame
+case class Frame(b1:Int, b2:Option[Int]) extends BowlingFrame
 case class SpareFrame(b1:Int) extends BowlingFrame {
-  val b2=Optional.of(10-b1)
+  val b2=Some(10-b1)
 }
 case object StrikeFrame extends BowlingFrame {
   val b1=10
-  val b2=Optional.empty[Int]
+  val b2=None
 }
-case class BonusFrame(b1:Int, b2:Optional[Int]) extends BowlingFrame
+case class BonusFrame(b1:Int, b2:Option[Int]) extends BowlingFrame
 
 object BowlingScorer {
   def scoreGame(game: String): Int = {
@@ -34,7 +32,7 @@ object BowlingScorer {
     val isSimpleFrame = "(\\d|-)(\\d|-)".r
     val isSpareFrame = "(\\d)/".r
     frame match {
-      case isSimpleFrame(b1,b2) => Frame(ballToInt(b1),Optional.of(ballToInt(b2)))
+      case isSimpleFrame(b1,b2) => Frame(ballToInt(b1),Some(ballToInt(b2)))
       case isSpareFrame(b1) => SpareFrame(ballToInt(b1))
       case "X" => StrikeFrame
     }
@@ -43,7 +41,7 @@ object BowlingScorer {
   def parseBonusFrame(frame:String) : BowlingFrame = {
     val isBonusFrame = "(\\d|-|X)(\\d|-|X)?".r
     frame match {
-      case isBonusFrame(b1,b2) => BonusFrame(ballToInt(b1), if (b2==null) Optional.empty() else Optional.of(ballToInt(b2)))
+      case isBonusFrame(b1,b2) => BonusFrame(ballToInt(b1), if (b2==null) None else Some(ballToInt(b2)))
     }
   }
 
